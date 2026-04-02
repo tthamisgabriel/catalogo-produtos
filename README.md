a61553fa35f6c5a3d88c03d911e395764e9253d4
Thamires Gabriel <tthamisgabriel@gmail.com>
2026-04-02 11:27:32 -0300

    projeto catalogo produtos

---
+++ README.md
@@ -0,0 +1,24 @@
+# Catálogo de Produtos
+
+Projeto simples feito em React para exibir um catálogo de produtos.
+
+## Funcionalidades
+
+- Exibe produtos na tela
+- Permite adicionar novos produtos
+- Valida campos do formulário
+- Simula carregamento com useEffect
+
+## Tecnologias usadas
+
+- React
+- Vite
+- JavaScript
+
+## Como rodar
+
+1. Instalar dependências:
+npm install
+
+2. Rodar o projeto:
+npm run dev
---
+++ src/pages/Home.jsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from "react";
+import ProdutoCard from "../components/ProdutoCard";
+
+function Home() {
+
+  const [produtos, setProdutos] = useState([]);
+  const [carregando, setCarregando] = useState(true);
+
+  const [nome, setNome] = useState("");
+  const [preco, setPreco] = useState("");
+  const [descricao, setDescricao] = useState("");
+
+  // SIMULAÇÃO DE API (EXATAMENTE O QUE O EXERCÍCIO PEDE)
+  useEffect(() => {
+    setTimeout(() => {
+      setProdutos([
+        {
+          id: 1,
+          nome: "Camiseta",
+          preco: 50,
+          descricao: "Camiseta básica",
+          imagem: "https://picsum.photos/100"
+        }
+      ]);
+
+      setCarregando(false);
+    }, 2000);
+  }, []);
+
+  function adicionarProduto(e) {
+    e.preventDefault();
+
+    if (!nome || !preco || !descricao) {
+      alert("Preencha todos os campos");
+      return;
+    }
+
+    const novoProduto = {
+      id: produtos.length + 1,
+      nome,
+      preco,
+      descricao,
+      imagem: "https://picsum.photos/100"
+    };
+
+    setProdutos([...produtos, novoProduto]);
+
+    setNome("");
+    setPreco("");
+    setDescricao("");
+  }
+
+  return (
+    <div className="container">
+      <h1>Produtos</h1>
+
+      {carregando ? (
+        <p>Carregando...</p>
+      ) : (
+        produtos.map((p) => (
+          <ProdutoCard
+            key={p.id}
+            nome={p.nome}
+            preco={p.preco}
+            descricao={p.descricao}
+            imagem={p.imagem}
+          />
+        ))
+      )}
+
+      <h2>Adicionar Produto</h2>
+
+      <form className="form" onSubmit={adicionarProduto}>
+        <input
+          placeholder="Nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+
+        <input
+          placeholder="Preço"
+          value={preco}
+          onChange={(e) => setPreco(e.target.value)}
+        />
+
+        <input
+          placeholder="Descrição"
+          value={descricao}
+          onChange={(e) => setDescricao(e.target.value)}
+        />
+
+        <button type="submit">Adicionar</button>
+      </form>
+    </div>
+  );
+}
+
+export default Home;
