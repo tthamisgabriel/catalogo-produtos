3970ff09a217999265a63e7660781f3e9483c6b9
Thamires Gabriel <tthamisgabriel@gmail.com>
2026-04-06 10:28:14 -0300

    detalhes finais

--- src/components/ProdutoCard.jsx
+++ src/components/ProdutoCard.jsx
@@ -1,16 +1,3 @@
-// function ProdutoCard({ nome, preco, descricao, imagem }) {
-//   return (
-//     <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
-//       <img src={imagem} width="100" />
-//       <h3>{nome}</h3>
-//       <p>Preço: R$ {preco}</p>
-//       <p>{descricao}</p>
-//     </div>
-//   );
-// }
-
-// export default ProdutoCard;
-
 function ProdutoCard({ nome, preco, descricao, imagem, onDelete }) {
   return (
     <div className="card">
